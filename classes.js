da86646b7b4d535d8e04b4c5d77525b1b00ef4b7
Rose Ames <rose@happyspork.com>
2014-11-06 21:30:30 -0500

    add methods

--- classes.js
+++ classes.js
@@ -12,17 +12,32 @@ function instantiate_class(cls) {
 }
 
 function get_attr(obj, attr_name) {
+  var ret;
   var attrs = obj['attrs'];
+
   if (attr_name in attrs) {
-    return attrs[attr_name];
+    ret = attrs[attr_name];
   }
-  return obj['class']['attrs'][attr_name]; // will be undefined if it's, you know, undefined.
+  else {
+    ret = obj['class']['attrs'][attr_name];
+  }
+
+  if (typeof(ret) === 'function') {
+    ret = bind_method(ret, obj);
+  }
+  return ret;
 }
 
 function set_attr(obj, attr_name, value) {
   obj['attrs'][attr_name] = value;
 }
 
+function bind_method(method, obj) {
+  return function(args) {
+    args.unshift(obj);
+    return method(args);
+  }
+} 
 
 var Foo = make_class({boop: 3});
 
@@ -31,5 +46,21 @@ var f2 = instantiate_class(Foo);
 
 set_attr(f, 'boop', 7);
 
-console.log(get_attr(f, 'boop')); // 7
-console.log(get_attr(f2, 'boop')); // 3
+console.log("This should be 7: ", get_attr(f, 'boop'));
+console.log("This should be 3: ", get_attr(f2, 'boop'));
+
+var Bar = make_class ({
+  "bam": 8,
+  "add2bam": function(args) {
+    self = args[0];
+    num2add = args[1];
+    return get_attr(self, 'bam') + num2add;
+  }
+});
+
+var b = instantiate_class(Bar);
+bound_method = get_attr(b, "add2bam");
+
+console.log("This should be 13: ", bound_method([5]));
+set_attr(b, 'bam', 9);
+console.log("This should be 14: ", bound_method([5]));
